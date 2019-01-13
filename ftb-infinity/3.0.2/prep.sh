86c0c964d4badb3150f66c70901ad20ad4a3757d
akoj93 <akoj93@gmail.com>
2019-01-13 18:46:53 +0100

    I djloköbjdob
    INSANITY

--- ftb-infinity/3.0.2/prep.sh
+++ ftb-infinity/3.0.2/prep.sh
@@ -6,4 +6,5 @@
 	sed -i "s/    \"backup_timer\".*/    \"backup_timer\": $backuptimer,/g"  /minecraft/local/ftbu/config.json && \
 	sed -i "s/    \"backups_to_keep\".*/    \"backups_to_keep\": $backupstokeep,/g"  /minecraft/local/ftbu/config.json && \
 	sed -i "s/      \"Welcome to the server!\".*/      \"$loginmotd\"/g"  /minecraft/local/ftbu/config.json && \
+	sed -i "s/    \"restart_timer\":.*/    \"restart_timer\": $restarttimer,/g"  /minecraft/local/ftbu/config.json && \
 	sed -i "s/\[\].*/[\n{\n\"uuid\": \"$adminuuid\",\n\"name\": \"$adminname\",\n\"level\": 4\n}\n]/g" ops.json
